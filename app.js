4e1ebc5ba2ea509546bab51b85f745ed338b8414
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2023-04-05 19:16:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 import paymentRoute from "./routes/paymentRoutes.js";
 import cors from 'cors';
 
-config({path:"./config/config.env"});
+config({path:"./etc/secrets/config.env"});
 
 export const app = express();
 
